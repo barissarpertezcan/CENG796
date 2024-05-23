73bc8f0b585edab549763751d642b537337283dd
Furkan Genç <gencfurkan2001@gmail.com>
2024-05-24 00:13:22 +0300

    Added train parameters

--- code/config.py
+++ code/config.py
@@ -7,7 +7,6 @@ BATCH_SIZE = 1
 root_dir = "../cc3m/train" # TODO: change root_dir with the path to the dataset according to your setup
 
 # training parameters
-first_epoch = 0
 num_train_epochs = 6
 Lambda = 1.0
 
@@ -20,9 +19,28 @@ adam_weight_decay = 1e-4
 adam_epsilon = 1e-8
 
 # checkpoint parameters
-output_dir = "../output"
+output_dir = "../output_1_resumed"
 save_steps = 1000
+
+# Load the models
+model_file = "data/v1-5-pruned.ckpt"  
+unet_file = "../output_1/best.pt"  # Set to None to finetune from scratch
 
 # EMA parameters
 ema_decay = 0.9999
 warmup_steps = 1000
+
+# TEXT TO IMAGE
+prompt1 = "A river with boats docked and houses in the background"
+prompt2 = "A piece of chocolate swirled cake on a plate"
+prompt3 = "A large bed sitting next to a small Christmas Tree surrounded by pictures"
+prompt4 = "A bear searching for food near the river"
+prompts = [prompt1, prompt2, prompt3, prompt4]
+uncond_prompt = ""  # Also known as negative prompt
+do_cfg = False
+cfg_scale = 8  # min: 1, max: 14
+
+# SAMPLER
+sampler = "ddpm"
+num_inference_steps = 50
+seed = 42
