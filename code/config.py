704405651b000717d33ae4257d9796f8c4cd7f46
Furkan Genç <gencfurkan2001@gmail.com>
2024-05-30 17:17:53 +0300

    Update configs for the final version

--- code/config.py
+++ code/config.py
@@ -4,11 +4,12 @@ HEIGHT = 512
 LATENTS_WIDTH = WIDTH // 8
 LATENTS_HEIGHT = HEIGHT // 8
 BATCH_SIZE = 1
-root_dir = "../cc3m/train" # TODO: change root_dir with the path to the dataset according to your setup
+root_dir = "../dataset/cc3m/train" # TODO: change root_dir with the path to the dataset according to your setup
 
 # training parameters
 num_train_epochs = 6
 Lambda = 1.0
+save_steps = 1000
 
 # optimizer parameters
 learning_rate = 1e-5
@@ -18,13 +19,12 @@ adam_beta2 = 0.999
 adam_weight_decay = 1e-4
 adam_epsilon = 1e-8
 
-# checkpoint parameters
-output_dir = "../output_1_resumed_2"
-save_steps = 1000
+# output directory
+output_dir = "../results/output_1"
 
 # Load the models
 model_file = "data/v1-5-pruned.ckpt"  
-unet_file = "../output_1_resumed/last.pt"  # Set to None to finetune from scratch
+unet_file = None  # Set to None to finetune from scratch
 
 # EMA parameters
 ema_decay = 0.9999
@@ -37,10 +37,14 @@ prompt3 = "A large bed sitting next to a small Christmas Tree surrounded by pict
 prompt4 = "A bear searching for food near the river"
 prompts = [prompt1, prompt2, prompt3, prompt4]
 uncond_prompt = ""  # Also known as negative prompt
-do_cfg = False
-cfg_scale = 8  # min: 1, max: 14
+do_cfg = True
+cfg_scale = 3  # min: 1, max: 14
+num_samples = 1
 
 # SAMPLER
 sampler = "ddpm"
 num_inference_steps = 50
 seed = 42
+
+# IMAGE TO TEXT
+test_dataset = "CIFAR10"
