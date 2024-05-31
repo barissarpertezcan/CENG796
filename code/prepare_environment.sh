62987943845083d4365264c662cd970e4d4015bf
Furkan Genç <gencfurkan2001@gmail.com>
2024-05-31 18:57:40 +0300

    Refactor the directories and remove unnecessary files

--- code/prepare_environment.sh
+++
@@ -1,4 +0,0 @@
-conda create -n DiffDis
-conda activate DiffDis
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-pip install -r requirements.txt
--- code/run.sh
+++
@@ -1,4 +0,0 @@
-bash prepare_environment.sh
-bash download_data.sh
-bash download_cc3m_dataset.sh
-bash extract_cc3m_dataset.sh
